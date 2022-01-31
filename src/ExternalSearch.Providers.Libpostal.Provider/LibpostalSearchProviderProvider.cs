ba8675808ccce03b993ea6683f913ad771165d68
Ben Fidge <ben.fidge@btopenworld.com>
2022-01-31 11:20:35 +0700

    For AB#7649

--- src/ExternalSearch.Providers.Libpostal.Provider/LibpostalSearchProviderProvider.cs
+++ src/ExternalSearch.Providers.Libpostal.Provider/LibpostalSearchProviderProvider.cs
@@ -111,6 +111,7 @@ namespace CluedIn.Provider.Libpostal
             throw new NotImplementedException();
         }
 
+        public override bool ScheduleCrawlJobs => false;
         public string Icon { get; } = Constants.Icon;
         public string Domain { get; } = Constants.Domain;
         public string About { get; } = Constants.About;
