e014ed801f7646428723ee237ff2728421f52d73
Yi-Wei <ywei390@gmail.com>
2024-11-20 15:26:08 +0800

    feat: Add guidance note to enricher configuration page

--- src/ExternalSearch.Providers.libpostal/Constants.cs
+++ src/ExternalSearch.Providers.libpostal/Constants.cs
@@ -10,6 +10,31 @@ namespace CluedIn.ExternalSearch.Providers.Libpostal
         public const string ComponentName = "Libpostal";
         public const string ProviderName = "Libpostal";
         public static readonly Guid ProviderId = Guid.Parse("aba4e4cf-3c48-4828-9fdf-990b22e1a29c");
+        public const string Instruction = """
+            [
+              {
+                "type": "bulleted-list",
+                "children": [
+                  {
+                    "type": "list-item",
+                    "children": [
+                      {
+                        "text": "Add the entity type to specify the golden records you want to enrich. Only golden records belonging to that entity type will be enriched."
+                      }
+                    ]
+                  },
+                  {
+                    "type": "list-item",
+                    "children": [
+                      {
+                        "text": "Add the vocabulary keys to provide the input for the enricher to search for additional information. For example, if you provide the website vocabulary key for the Web enricher, it will use specific websites to look for information about companies. In some cases, vocabulary keys are not required. If you don't add them, the enricher will use default vocabulary keys."
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+            """;
         public struct KeyName
         {
             public const string AcceptedEntityType = "acceptedEntityType";
